e5bf8147cf200e33ddbb4bdfd4452f3082d194e2
cly4auHo <arsen.dmitriev96@gmail.com>
2020-07-09 18:58:23 +0300

    last task

---
+++ StopWatch3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace StopWatch3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(93, 12);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(174, 12);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 2;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 41);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(237, 20);
+            this.textBox.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "StopWatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ StopWatch3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace StopWatch3
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopWatch;
+        private TimeSpan ts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stopWatch = new Stopwatch();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            stopWatch.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            textBox.Text = ts.ToString();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            stopWatch.Restart();
+            textBox.Text = "";
+        }
+    }
+}
