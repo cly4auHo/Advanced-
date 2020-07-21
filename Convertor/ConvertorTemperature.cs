081831e1ddfa1c45929b10a447876717cfe5be89
cly4auHo <arsen.dmitriev96@gmail.com>
2020-07-21 05:57:51 +0300

    init

---
+++ Convertor/ConvertorTemperature.cs
@@ -0,0 +1,67 @@
+﻿namespace Convertor
+{
+    public class ConvertorTemperature
+    {
+        private const double offsetCelsiusKelvin = 273.15;
+        private const double offsetCelsiusFahrenheit = 32;
+        private const double coefficientCelsiusFahrenheit = 1.8;
+
+        private const double minValueFahrenheit = -459.67;
+        private const double minValueCelsius = -273.15;
+        private const double minValueKelvin = 0;
+
+        //K = C - 273.15
+        public double CelsiusToKelvin(double temperatureInCelsius)
+        {
+            if (temperatureInCelsius < minValueCelsius)
+                temperatureInCelsius = minValueCelsius;
+
+            return temperatureInCelsius - offsetCelsiusKelvin;
+        }
+
+        //C = K + 273.15
+        public double KelvinToCelsius(double temperatureInKelvin)
+        {
+            if (temperatureInKelvin < minValueKelvin)
+                temperatureInKelvin = minValueKelvin;
+
+            return temperatureInKelvin + offsetCelsiusKelvin;
+        }
+
+        //F = (K - 273.15) * 1.8 + 32
+        public double KelvinToFahrenheit(double temperatureInKelvin)
+        {
+            if (temperatureInKelvin < minValueKelvin)
+                temperatureInKelvin = minValueKelvin;
+
+            return (temperatureInKelvin - offsetCelsiusKelvin) * coefficientCelsiusFahrenheit + offsetCelsiusFahrenheit;
+        }
+
+        //K = (F - 32) / 1.8 + 273.15
+        public double FahrenheitToKelvin(double temperatureInFahrenheit)
+        {
+            if (temperatureInFahrenheit < minValueFahrenheit)
+                temperatureInFahrenheit = minValueFahrenheit;
+
+            return (temperatureInFahrenheit - offsetCelsiusFahrenheit) / coefficientCelsiusFahrenheit + offsetCelsiusKelvin;
+        }
+
+        //F = C * 1.8 + 32
+        public double CelsiusToFahrenheit(double temperatureInCelsius)
+        {
+            if (temperatureInCelsius < minValueCelsius)
+                temperatureInCelsius = minValueCelsius;
+
+            return temperatureInCelsius * coefficientCelsiusFahrenheit + offsetCelsiusFahrenheit;
+        }
+
+        //C = (F - 32) / 1.8
+        public double FahrenheitToCelsius(double temperatureInFahrenheit)
+        {
+            if (temperatureInFahrenheit < minValueFahrenheit)
+                temperatureInFahrenheit = minValueFahrenheit;
+
+            return (temperatureInFahrenheit - offsetCelsiusFahrenheit) / coefficientCelsiusFahrenheit;
+        }
+    }
+}
---
+++ Home6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Home6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load("Convertor");
+                Type type = assembly.GetType("Convertor.ConvertorTemperature");
+                MethodInfo methodAcceleration = type.GetMethod("CelsiusToFahrenheit");
+                double? t = methodAcceleration.Invoke(Activator.CreateInstance(type), new object[] { 70.6 }) as double?;
+                Console.WriteLine(t);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
