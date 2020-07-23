6691d5eeb869e4e3dc09f48af1f5788032df3d32
cly4auHo <arsen.dmitriev96@gmail.com>
2020-07-23 16:34:00 +0300

    init 1 and 2 task

---
+++ Home7/FoldersCreator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Home7
+{
+    public class FoldersCreator
+    {
+        private static List<string> paths = new List<string>();
+        private const string testFolderPath = @"D:\Test\Folder";
+
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                string path = testFolderPath + i;
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                    paths.Add(path);
+
+                    Console.WriteLine("Список каталогов: ");
+
+                    foreach (string item in Directory.GetDirectories(path))
+                        Console.WriteLine(item);
+
+                    Console.WriteLine("Cписок файлов: ");
+
+                    foreach (string item in Directory.GetFiles(path))
+                        Console.WriteLine(item);
+
+                    Console.WriteLine("Родительский каталог: " + Directory.GetParent(path));
+                }
+            }
+
+            Console.WriteLine("Press any button to delete Directories");
+            Console.ReadKey();
+
+            foreach (string path in paths)
+                if (Directory.Exists(path))
+                    Directory.Delete(path);
+        }
+    }
+}
---
+++ Home7/WriterReaderFile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Home7
+{
+    public class WriterReaderFile
+    {
+        private static char[] wrongCharactersForNameFile = { '/', '\\', ':', '*', '?', '«', '<', '>', '|' };
+        private const string testFolderPath = @"D:\Test\";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write name of file");
+            string path;
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                name = name.Trim();
+
+                if (CheckPathHaveWrongChar(name))
+                {
+                    Console.WriteLine("Wrong characters");
+                    continue;
+                }
+                else if (string.IsNullOrEmpty(name))
+                {
+                    name = "DEFAULT_NAME";
+                }
+
+                path = testFolderPath + name + ".txt";
+                break;
+            }
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    Console.WriteLine("Press CTRL + Enter to end");
+
+                    while (true)
+                    {
+                        sw.WriteLine(Console.ReadLine());
+
+                        if (Console.ReadKey(true).Modifiers.HasFlag(ConsoleModifiers.Control))
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any button to open file");
+            Console.ReadKey();
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string text;
+
+                    while ((text = sr.ReadLine()) != null)
+                        Console.WriteLine(text);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool CheckPathHaveWrongChar(string path)
+        {
+            foreach (char item in wrongCharactersForNameFile)
+                if (path.Contains(item))
+                    return true;
+
+            return false;
+        }
+    }
+}
