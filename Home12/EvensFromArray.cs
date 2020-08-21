f818afbe62ba0c790896069e17ed9fc24bd47800
cly4auHo <arsen.dmitriev96@gmail.com>
2020-08-21 12:15:29 +0300

    init

---
+++ Home12/EvensFromArray.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Home12
+{
+    public class EvensFromArray
+    {
+        public int[] GetEvenValues(int[] array)
+        {           
+            return array.AsParallel().Where(x => x % 2 == 0).ToArray();
+        }      
+    }
+}
---
+++ Home12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Home12
+{
+    public class Program
+    {
+        private static Thread thread1;
+        private static Thread thread2;
+        private static Thread main;
+
+        public static void Main(string[] args)
+        {
+            main = Thread.CurrentThread;
+            thread1 = new Thread(ThreadMethod1);
+            thread2 = new Thread(ThreadMethod2);
+
+            Parallel.Invoke(Method1, Method2);
+        }
+
+        private static void Method1()
+        {
+            thread1.Start();
+            main.Join();
+        }
+
+        private static void Method2()
+        {
+            thread2.Start();
+            main.Join();
+        }
+
+        private static void ThreadMethod1()
+        {
+
+        }
+
+        private static void ThreadMethod2()
+        {
+
+        }
+    }
+}
